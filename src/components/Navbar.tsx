19e09f6dcfc9d593c2704267fd8c906f401787db
vinnidias <vinicius.3991@gmail.com>
2023-09-01 15:19:56 -0300

    navbar created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import avatar from "../assets/avatar-de-perfil.png";
+
+export const Navbar = () => {
+  return (
+    <nav className="flex justify-between w-screen h-[8vh] px-10 py-2 shadow-sm">
+      <div>
+        <h1 className="text-base">Nome da Clínica</h1>
+        <p className="text-sm">Nome do Colaborador</p>
+      </div>
+      <Image src={avatar} alt="foto de perfil" width={40} height={40} className="rounded-full bg-gray-300"/>
+    </nav>
+  );
+};
