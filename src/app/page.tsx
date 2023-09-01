7c3e674c41c94988a62ae2025257608b6a132805
vinnidias <vinicius.3991@gmail.com>
2023-09-01 17:58:04 -0300

    using dashboard and warning components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import { Dashboard } from "@/components/Dashboard";
+import { Warnings } from "@/components/Warnings";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>área de trabalho</h1>
+    <main className="flex min-h-screen pt-[8.2vh] pl-12 md:pl-36">
+      <section className="flex flex-col gap-2 p-6 w-[100%] md:w-[60%]">
+        <Dashboard />
+        <Warnings />
+      </section>
     </main>
-  )
+  );
 }
