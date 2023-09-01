fa440d3d01894e5f5af82fbe7ce4dca60b3f8332
vinnidias <vinicius.3991@gmail.com>
2023-09-01 18:00:25 -0300

    adding inital pages setup

---
+++ src/app/scheduling/page.tsx
@@ -0,0 +1,7 @@
+export default function Scheduling() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Agendamento</h1>
+    </main>
+  );
+}
