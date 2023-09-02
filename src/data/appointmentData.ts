255539f7049821bf76d484ee9878fac1523bcbf3
vinnidias <vinicius.3991@gmail.com>
2023-09-02 17:17:51 -0300

    mock data

---
+++ src/data/appointmentData.ts
@@ -0,0 +1,37 @@
+export const appointmentData = [
+  {
+    patient: "Manoel Dias",
+    doctor: "Romeu Santos",
+    date: "01/09/2023",
+    hour: "08:30",
+    value: "R$ 150,00",
+  },
+  {
+    patient: "Maria Fonseca",
+    doctor: "Romeu Santos",
+    date: "01/09/2023",
+    hour: "10:30",
+    value: "R$ 250,00",
+  },
+  {
+    patient: "Noel Rosa",
+    doctor: "Julia Prates",
+    date: "01/09/2023",
+    hour: "10:30",
+    value: "R$ 250,00",
+  },
+  {
+    patient: "Marcos Souza",
+    doctor: "Julia Prates",
+    date: "01/09/2023",
+    hour: "12:00",
+    value: "R$ 150,00",
+  },
+  {
+    patient: "Noele Silva",
+    doctor: "Romeu Santos",
+    date: "01/09/2023",
+    hour: "15:30",
+    value: "R$ 150,00",
+  },
+];
